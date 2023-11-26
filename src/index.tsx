14f4b07190f050de9bd9b14e78a9a91a3d433902
working-zima <working.zima@gmail.com>
2023-11-26 16:58:37 +0900

    feat: Introduction to Recoil

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
+import { RecoilRoot } from "recoil";
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>,
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </RecoilRoot>,
   document.getElementById("root")
 );
