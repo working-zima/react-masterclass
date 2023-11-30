05b863092f370c6c3a45ede0c6d48b7078b58fe3
working-zima <working.zima@gmail.com>
2023-11-30 18:25:54 +0900

    feat: MotionValues part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion, useMotionValue } from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -19,11 +19,11 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
+  const scale = useTransform(x, [-600, 0, 600], [2, 1, 0.1]);
 
   return (
     <Wrapper>
-      <button onClick={() => x.set(200)}>click me</button>
-      <Box style={{ x }} drag="x" dragSnapToOrigin />
+      <Box style={{ x, scale: scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
